4b88c01d2f3bee16ab129be1cde2be888df98491
son <handsun000@gmail.com>
2024-12-10 15:30:10 +0900

    WiseSayings findAll

---
+++ src/main/java/com/ll/basic/domain/wiseSaying/wiseSaying/service/WiseSayingService.java
@@ -0,0 +1,25 @@
+package com.ll.basic.domain.wiseSaying.wiseSaying.service;
+
+import com.ll.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class WiseSayingService {
+    private final List<WiseSaying> wiseSayings;
+    private long lastId;
+
+    public WiseSayingService() {
+        this.wiseSayings = new ArrayList<>() {{
+            add(WiseSaying.builder().id(++lastId).content("삶이 있는 한").author("작가1").build());
+            add(WiseSaying.builder().id(++lastId).content("니가 있는 한").author("작가2").build());
+        }};
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings.stream()
+                .sorted(Comparator.comparing(WiseSaying::getId).reversed())
+                .toList();
+    }
+}
