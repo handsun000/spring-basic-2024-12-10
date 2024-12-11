ef2e9e4c3d2e9a44bdb0443a3ab0028f2d7d1df6
son <handsun000@gmail.com>
2024-12-11 11:10:15 +0900

    @Configuration + @Bean

---
+++ src/main/java/com/ll/basic/domain/global/initData/BeanInitData.java
@@ -0,0 +1,15 @@
+package com.ll.basic.domain.global.initData;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class BeanInitData {
+
+    @Bean
+    public List<Integer> ages() {
+        return List.of(10,20,30,40,50);
+    }
+}
